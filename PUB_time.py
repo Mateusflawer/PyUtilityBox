d190d13ab9d9e1c142d24cf1efe5d541d057ecf8
Mateus Alves <111348801+Mateusflawer@users.noreply.github.com>
2024-08-23 17:17:28 -0300

    Update PUB_time.py

--- PUB_time.py
+++ PUB_time.py
@@ -3,7 +3,7 @@ import time
 def countdown_timer(wait_seconds: int, msg_wait=""):
     """Countdown by seconds"""
     for i in range(wait_seconds, 0, -1):
-        print(f"{msg_wait} {i}second(s) ...", end='\r')
+        print(f"{msg_wait} {i} second(s) ...", end='\r')
         time.sleep(1)  # Waits for 1 second
         
 
