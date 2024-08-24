7f54aae5d910b0d80176f3819b848307e60b72f5
Mateus Alves <111348801+Mateusflawer@users.noreply.github.com>
2024-08-24 09:54:19 -0300

    Update PUB_time.py

--- PUB_time.py
+++ PUB_time.py
@@ -7,28 +7,29 @@ def countdown_timer(wait_seconds: int, msg_wait=""):
         time.sleep(1)  # Waits for 1 second
         
 
-def execute_with_retries(max_retries: int, function, *args, **kwargs):
+def retry_on_failure(max_retries: int):
     """
-    Executes a function with a specified number of retries in case of failure.
+    Decorator that retries the execution of a function in case of failure.
     
     Parameters:
         max_retries (int): Maximum number of retry attempts.
-        function (callable): Function to be executed.
-        *args: Positional arguments to pass to the function.
-        **kwargs: Keyword arguments to pass to the function.
     
     Returns:
-        The result of the function if successful, None otherwise.
+        The decorated function which will be executed with retries.
     """
-    attempts = 0
-    while attempts < max_retries:
-        try:
-            return function(*args, **kwargs)
-        except Exception as e:
-            attempts += 1
-            print(f"Attempt {attempts}/{max_retries} failed with error: {e}")
-            if attempts < max_retries:
-                time.sleep(5 * attempts)  # Exponential backoff
-            else:
-                print("All retry attempts failed.")
-                return None
+    def decorator(function):
+        def wrapper(*args, **kwargs):
+            attempts = 0
+            while attempts < max_retries:
+                try:
+                    return function(*args, **kwargs)
+                except Exception as e:
+                    attempts += 1
+                    print(f"Attempt {attempts}/{max_retries} failed with error: {e}")
+                    if attempts < max_retries:
+                        time.sleep(5 * attempts)  # Exponential backoff
+                    else:
+                        print("All retry attempts failed.")
+                        return None
+        return wrapper
+    return decorator
