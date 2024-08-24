450811eb12985baffa143553e5e0c02380b57047
Mateus Alves <111348801+Mateusflawer@users.noreply.github.com>
2024-08-24 09:32:26 -0300

    Update PUB_input.py

--- PUB_input.py
+++ PUB_input.py
@@ -1,14 +1,26 @@
-def input_period_int(msg_desc: str, msg_info: str = None, msg_error: str = None) -> list:
+def input_period_int(msg_desc: str, msg_info: str = None, msg_error: str = "Entrada inválida. Tente novamente.") -> int:
+    """
+    Solicita a entrada de um número inteiro positivo do usuário e retorna esse valor.
+    
+    Parameters:
+        msg_desc (str): Mensagem descritiva para solicitar a entrada.
+        msg_info (str, optional): Mensagem de informação a ser exibida antes da entrada. Padrão é None.
+        msg_error (str, optional): Mensagem de erro a ser exibida em caso de entrada inválida. Padrão é "Entrada inválida. Tente novamente."
+    
+    Returns:
+        int: O número inteiro fornecido pelo usuário.
+    """
     while True:
+        if msg_info:
+            print(f"INFO - {msg_info}")
+        
+        dias = input(msg_desc).strip()
+        
         try:
-            if msg_info is not None:
-                print(f"INFO - {msg_info}")
-            dias = input(msg_desc)
-            dias_int = int(dias.strip())
+            dias_int = int(dias)
             if dias_int == 0:
-                break
-        except Exception as e:
-            if msg_error is not None:
-                print(msg_error)
-        else:
+                print("INFO - Valor zero inserido. Saindo.")
+                return 0
             return dias_int
+        except ValueError:
+            print(f"ERROR - {msg_error}")
